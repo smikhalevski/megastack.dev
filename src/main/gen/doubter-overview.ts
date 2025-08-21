0118927542954e681a324c60cba265d7ee8d1a38
smikhalevski <smikhalevski@gmail.com>
2025-08-21 11:49:48 +0300

    Updated docs

--- src/main/gen/doubter-overview.ts
+++ src/main/gen/doubter-overview.ts
@@ -1,5 +1,1 @@
-export default {
-  version: '5.2.0',
-  overviewContent:
-    '<p>Runtime validation and transformation library.</p><ul><li>TypeScript first.</li><li>Sync and async validation and transformation flows.</li><li>Circular object references support.</li><li>Collect all validation issues, or exit early.</li><li>Runtime type introspection.</li><li>Human-oriented type coercion.</li><li>High performance and low memory consumption.</li><li>Zero dependencies.</li><li>Pluggable architecture.</li><li>Compatible with Standard Schema.</li><li>Tree-shakable. 3 — 12 kB gzipped depending on what features you use.</li><li>Check out the Cookbook for real-life examples!</li></ul>',
-};
+export default {"version":"5.2.0","overviewContent":"<p>Runtime validation and transformation library.</p><ul><li>TypeScript first.</li><li>Sync and async validation and transformation flows.</li><li>Circular object references support.</li><li>Collect all validation issues, or exit early.</li><li>Runtime type introspection.</li><li>Human-oriented type coercion.</li><li>High performance and low memory consumption.</li><li>Zero dependencies.</li><li>Pluggable architecture.</li><li>Compatible with Standard Schema.</li><li>Tree-shakable. 3 — 12 kB gzipped depending on what features you use.</li><li>Check out the Cookbook for real-life examples!</li></ul>"}
--- src/main/gen/react-corsair-overview.ts
+++ src/main/gen/react-corsair-overview.ts
@@ -1,5 +1,1 @@
-export default {
-  version: '0.0.10',
-  overviewContent:
-    '<p>Type-safe router that abstracts URLs away.</p><ul><li>TypeScript first: type-safe path and query parameters.</li><li>Code splitting, data loading and prefetching out-of-the box.</li><li>Route interception and inline routes.</li><li>Expressive and concise API with strict typings.</li><li>Supports SSR, partial pre-rendering and Suspense.</li><li>Optional history integration.</li><li>Just 9 kB gzipped.</li><li>Check out the Cookbook for real-life examples!</li></ul>',
-};
+export default {"version":"0.0.11","overviewContent":"<p>Type-safe router that abstracts URLs away.</p><ul><li>TypeScript first: type-safe path and query parameters.</li><li>Code splitting, data loading and prefetching out-of-the box.</li><li>Route interception and inline routes.</li><li>Expressive and concise API with strict typings.</li><li>Supports SSR, partial pre-rendering and Suspense.</li><li>Optional history integration.</li><li>Just 9 kB gzipped.</li><li>Check out the Cookbook for real-life examples!</li></ul>"}
